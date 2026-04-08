62ac5a702d9279e3a0489f9f514872be11690192
Tena Rosenblum <tenarosenblum@tenas-air.myfiosgateway.com>
2026-04-08 14:53:15 -0400

    restore env variables

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://esgfbteyvbhlusuzssgq.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVzZ2ZidGV5dmJobHVzdXpzc2dxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU1NjQ3MDMsImV4cCI6MjA5MTE0MDcwM30.Z6fem95bMS0UQDOISHn95VFFh-r3Y8YgiYfUe10i6ts'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-console.log('Supabase loaded')
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
