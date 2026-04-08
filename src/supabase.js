9c2e54182769890abcfae193f3c8656e645d1e46
Tena Rosenblum <tenarosenblum@tenas-air.myfiosgateway.com>
2026-04-08 14:49:16 -0400

    hardcode supabase keys to test

--- src/supabase.js
+++ src/supabase.js
@@ -1,9 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = 'https://esgfbteyvbhlusuzssgq.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVzZ2ZidGV5dmJobHVzdXpzc2dxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU1NjQ3MDMsImV4cCI6MjA5MTE0MDcwM30.Z6fem95bMS0UQDOISHn95VFFh-r3Y8YgiYfUe10i6ts'
 
-console.log('Supabase URL:', supabaseUrl)
-console.log('Supabase Key:', supabaseAnonKey)
-
+console.log('Supabase loaded')
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
